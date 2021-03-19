3a9b214970a70dd7963201d7390e95bbee351175
simontrixl <simon.trixl@gmial.com>
2021-03-19 10:49:12 +0100

    correct mistake

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -1,4 +1,4 @@
-/ Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
 console.log('Codecademy'.toUpperCase());
 
 // Use a string method to log the following string without whitespace at the beginning and end of it.
