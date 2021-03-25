4391e374f4d1723ce2c4e28746edb71d46dc2dd2
simontrixl <simon.trixl@gmial.com>
2021-03-25 16:14:36 +0100

    open street map tile

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,10 @@
-const map = L.map("map");
+const map = L.map("map" ,{
+    center: [ -43.881111, 169.042222 ],
+    zoom: 13, 
+    layers: [
+        L.tileLayer ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+});
+//-43.881111, 169.042222
+
 console.log(document.querySelector("#map"));
