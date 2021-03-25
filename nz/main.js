ee1e6111424ecb7a3dab701f9b07a6b3fd6ec16f
simontrixl <simon.trixl@gmial.com>
2021-03-25 16:20:02 +0100

    marker hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -7,4 +7,6 @@ const map = L.map("map" ,{
 });
 //-43.881111, 169.042222 Koordinaten von Haast
 
+let mrk =L.marker([-43.881111, 169.042222]).addTo(map);
+
 console.log(document.querySelector("#map"));
