4c2f0fc68eed913dc01ebbcbff5c8347d450e20e
simontrixl <simon.trixl@gmial.com>
2021-04-15 16:46:07 +0200

    coordinates

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -14,4 +14,27 @@ let layerControl = L.control.layers({
     "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
     "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
     "BasemapAT.highdpi": L.tileLayer.provider('BasemapAT.highdpi'),
+    "BasemapAT.overlay": L.tileLayer.provider('BasemapAT.overlay'),
+    "Basemap.overlay+orthofoto": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay'),
+    ])
 }).addTo(map);
+
+
+
+let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson' ;
+
+fetch(awsUrl)
+then(response => response.json())
+.then(json => {
+    console.log('Daten konvertiert: ', json);
+    for (station of json.features) {
+        console.log('Station: ', station);
+        let marker = L.marker(
+            [station.geometry.coordinates[1],
+            station.geometry.coordinates[0]]
+        );
+        marker.addTo(map);
+    }
+});
