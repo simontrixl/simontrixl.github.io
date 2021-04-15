dc974f1362b9d24d68af1b257b6ef67266c584bd
simontrixl <simon.trixl@gmial.com>
2021-04-15 16:07:26 +0200

    load more basemaps

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -11,5 +11,7 @@ let map = L.map("map", {
 
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
-    "BasemapAT.orthofoto": L.tilelayer.provider('BasemapAT.orthofoto')
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
+    "BasemapAT.highdpi": L.tileLayer.provider('BasemapAT.highdpi'),
 }).addTo(map);
