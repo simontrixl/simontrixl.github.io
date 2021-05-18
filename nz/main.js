ae84b703feb68e34106ec3814c492cff9e7b9699
simontrixl <simon.trixl@gmial.com>
2021-05-18 11:31:09 +0200

    correct minimap

--- nz/main.js
+++ nz/main.js
@@ -57,7 +57,7 @@ nav.onchange = (evt) => {
 
 //minimap
 var miniMap = new L.Control.MiniMap(
-    L.tileLayer.provider("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"), {
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"), {
         toggleDisplay: true,
         minimized: false,
     }
