d790b9aabd3710430ede972c9d99b635a21c0100
simontrixl <simon.trixl@gmial.com>
2021-05-18 11:29:44 +0200

    fullscreen and minimap

--- nz/main.js
+++ nz/main.js
@@ -12,6 +12,7 @@ let stop = {
 const map = L.map("map", {
     //center: [ stop.lat, stop.lng ],
     //zoom: 13, 
+    fullscreenControl: true,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
